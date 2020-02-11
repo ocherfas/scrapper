3fabdd1eef5b60c5dd4770e6faf492b9df6762be
Or Cherfas <or@run.ai>
2020-02-11 23:07:53 +0200

    option to configure email publisher

--- src/emailPublisher.ts
+++ src/emailPublisher.ts
@@ -2,14 +2,30 @@ import {IPublisher} from './interfaces'
 import factory from 'sendmail'
 const sendmail = factory({silent: true})
 
+export type EmailPublisherOptions = {
+    to: string,
+    from?: string,
+    subject?: string
+}
+
+const defaults = {
+    from: "notifier@notifier.com",
+    subject: "New Transaction"
+}
+
 export default class EmailPublisher implements IPublisher{
+    options: EmailPublisherOptions
+
+    constructor(options: EmailPublisherOptions){
+        this.options = {...defaults, ...options}
+    }
 
     publish(date: Date, description: string, chargedAmount: number) {
         return new Promise<void>((resolve, reject) => {
             sendmail({
-                from: "notifier@notifier.com",
-                to: "ocherfas@gmail.com",
-                subject: "New Transaction",
+                from: this.options.from,
+                to: this.options.to,
+                subject: this.options.subject,
                 html: `Transaction with amount ${chargedAmount} with description '${description}' was received on ${date}`,
             }, (err, domain) => {
                 if (err != null) {
