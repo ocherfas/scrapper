941d084a766a5794fa2e5b1c1a514a6cf69ca429
Or Cherfas <or@run.ai>
2020-04-27 21:50:11 +0300

    close browser instance after scrapping is finished

--- src/israeliScrapers.ts
+++ src/israeliScrapers.ts
@@ -4,16 +4,26 @@ const puppeteer = require('puppeteer')
 
 class IsraeliScrapers implements IIsraeliScrapper{
     async scrape(options: any, credentials: Credentials) {
-        const optionsWithBrowser = {
-            ...options,
-            browser: await puppeteer.launch({
-                args: [
-                    '--no-sandbox'
-                ]
-            })
+        const browser = await puppeteer.launch({
+            args: [
+                '--no-sandbox'
+            ]
+        })
+
+        try {
+            const optionsWithBrowser = {
+                ...options,
+                browser: await puppeteer.launch({
+                    args: [
+                        '--no-sandbox'
+                    ]
+                })
+            }
+            const scraper = createScraper(optionsWithBrowser, credentials)    
+            return scraper.scrape(credentials) as Promise<ScrapeResult>
+        } finally {
+            await browser.close()
         }
-        const scraper = createScraper(optionsWithBrowser, credentials)
-        return scraper.scrape(credentials) as Promise<ScrapeResult>
     }
 }
 
