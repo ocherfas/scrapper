f734609f6c7969719738f6616aa5f1ca9de78ac7
Or Cherfas <or@run.ai>
2020-04-28 18:06:09 +0300

    revert back to using custom chromium init file instead of creating browswer instance ourselves

--- Dockerfile
+++ Dockerfile
@@ -10,8 +10,9 @@ RUN apk add --no-cache \
       nodejs \
       npm
 
+COPY ./hack/chrome /usr/bin/chromium-browser-custom
 ENV PUPPETEER_SKIP_CHROMIUM_DOWNLOAD=true \
-    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser \
+    PUPPETEER_EXECUTABLE_PATH=/usr/bin/chromium-browser-custom \
     CONFIG_PATH=config.json
 
 WORKDIR /scrapper
--- src/israeliScrapers.ts
+++ src/israeliScrapers.ts
@@ -1,29 +1,10 @@
 import {IIsraeliScrapper, Credentials, ScrapeResult} from './interfaces'
 const {createScraper} = require('israeli-bank-scrapers')
-const puppeteer = require('puppeteer')
 
 class IsraeliScrapers implements IIsraeliScrapper{
     async scrape(options: any, credentials: Credentials) {
-        const browser = await puppeteer.launch({
-            args: [
-                '--no-sandbox'
-            ]
-        })
-
-        try {
-            const optionsWithBrowser = {
-                ...options,
-                browser: await puppeteer.launch({
-                    args: [
-                        '--no-sandbox'
-                    ]
-                })
-            }
-            const scraper = createScraper(optionsWithBrowser, credentials)    
-            return scraper.scrape(credentials) as Promise<ScrapeResult>
-        } finally {
-            await browser.close()
-        }
+        const scraper = createScraper(options, credentials)    
+        return scraper.scrape(credentials) as Promise<ScrapeResult>
     }
 }
 
